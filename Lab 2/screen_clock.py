6c69d91dcf976c114fcac3e195aaa63be5a37f67
Alubeto <rh652@cornell.edu>
2025-09-15 17:03:46 -0400

    screen_clock fix

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -65,6 +65,7 @@ while True:
     draw.rectangle((0, 0, width, height), outline=0, fill=400)
 
     #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py 
+    draw.text((x, top), time.strftime("%m/%d/%Y %H:%M:%S"), font=font, fill="#FFFFFF")
 
     # Display image.
     disp.image(image, rotation)
